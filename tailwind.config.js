bfd2d053e4a4eba8320192c3b53ead439723663b
VIGNESH K S <iamvvv0@gmail.com>
2021-06-26 11:49:54 +0530

    Install Tailwind CSS #1

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/*.svelte", "./src/*.html", "./src/*.css"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
