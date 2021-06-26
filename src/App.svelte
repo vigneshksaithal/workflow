7ae873bd7d39f2131be7c2709554c874e9306cd4
VIGNESH K S <iamvvv0@gmail.com>
2021-06-26 19:28:31 +0530

    Add page, head

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import router from "page";
+  import "tailwindcss/tailwind.css";
+  import Head from "./Head.svelte";
+  import Homepage from "./Homepage.svelte";
+
+  let page;
+
+  router("/", () => (page = Homepage));
+
+  router.start();
+</script>
+
+<svelte:head>
+  <Head />
+</svelte:head>
+<!-- <Head /> -->
+
+<div class="container-sm px-3 max-w-3xl mx-auto mt-10">
+  <svelte:component this={page} />
+</div>
