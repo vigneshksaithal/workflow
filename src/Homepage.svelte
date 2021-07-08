d5046de377f81aa768c9b714125553525c2e2c7e
VIGNESH K S <iamvvv0@gmail.com>
2021-07-08 23:02:50 +0530

    Change: 1. Add div based custom progress bar 2. Make task input col-sapn-11 3. Update paddings to have sufficient white space between elements

--- src/Homepage.svelte
+++ src/Homepage.svelte
@@ -1,7 +1,7 @@
 <script>
   import { tweened } from "svelte/motion";
-  let original = 10 * 60; // TYPE NUMBER OF SECONDS HERE
-  let timer = tweened(original);
+  let initialTime = 25 * 60; // TYPE NUMBER OF SECONDS HERE
+  let timer = tweened(initialTime);
 
   function startTimer() {
     setInterval(() => {
@@ -30,25 +30,42 @@
 </script>
 
 <div class="">
-  <div class="border-2">
+  <div class="card border-2">
     <div class="grid grid-cols-12 px-2 py-2">
-      <span class="col-span-8 text-4xl">{minutes}:{seconds}</span>
-      <button class="col-span-4 btn-primary" on:click={startTimer}>START</button
+      <span class="col-span-10 text-4xl">{minutes}:{seconds}</span>
+      <button class="col-span-2 w-full btn-primary" on:click={startTimer}
+        >START</button
       >
     </div>
-    <progress class="w-full bg-indigo-600" value={$timer / original} />
+    <!-- progress bar -->
+    <div class="h-3 relative w-full rounded-full overflow-hidden mt-2">
+      <div class="w-full h-full bg-gray-200 absolute" />
+      <div
+        id="bar"
+        class="h-full bg-green-500 relative"
+        style="width: {($timer / initialTime) * 100}%;"
+      />
+    </div>
+
+    <!-- <progress class="w-full bg-indigo-600" value={$timer / original} /> -->
   </div>
 
-  <div class="card px-4 mb-6">
-    <div class="mb-4">
-      <input class="input-text w-full" type="text" placeholder="Title" />
+  <div class="card px-6 mt-4 mb-6">
+    <div class="grid grid-cols-12 gap-4 mb-5">
+      <input
+        class="col-span-11 input-text w-full"
+        type="text"
+        placeholder="Title"
+      />
+      <!-- <button class="col-span-1">Date</button> -->
+      <!-- timer dropdown -->
     </div>
 
     {#each tasksInput.subTasks as subTask}
       <div class="grid grid-cols-12">
         <input class="col-span-1" type="checkbox" />
         <input
-          class="col-span-10 my-2"
+          class="col-span-11 my-2"
           type="text"
           bind:value={subTask.title}
         />
