fc8d29aa2479b959534747ea09f903e57125f60c
VIGNESH K S <iamvvv0@gmail.com>
2021-07-09 20:33:26 +0530

    Add responsive navbar 😊

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,18 @@
 <script>
-  import router from "page";
-  import "tailwindcss/tailwind.css";
-  import Head from "./Head.svelte";
-  import Homepage from "./Homepage.svelte";
+  import router from 'page';
+  import 'tailwindcss/tailwind.css';
+  import Head from './Head.svelte';
+  import Homepage from './Homepage.svelte';
 
   let page;
 
-  router("/", () => (page = Homepage));
+  router('/', () => (page = Homepage));
 
   router.start();
 </script>
 
-<!-- <Head /> -->
+<Head />
 
-<div class="container-sm px-3 max-w-3xl mx-auto mt-10">
+<div class="container px-3 max-w-3xl mx-auto mt-2">
   <svelte:component this={page} />
 </div>
