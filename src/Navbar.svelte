5fb9c5782ba1263dc0108368e10e079eb68dbdd1
VIGNESH K S <iamvvv0@gmail.com>
2021-07-10 11:26:28 +0530

    Reduce code

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header
-  class="flex flex-wrap flex-row justify-between md:items-center md:space-x-4 bg-white py-6 px-6 relative"
+  class="border-b-2 border-green-400 flex flex-wrap flex-row justify-between md:items-center md:space-x-4 bg-white py-3 px-6 relative"
 >
   <a href="/" class="block">
     <span class="sr-onl font-semibold tracking-wider">Workflow</span>
@@ -18,7 +18,7 @@
     on:click={() => {
       mobileMenuOpen = !mobileMenuOpen;
     }}
-    class="inline-block md:hidden w-12 h-10 bg-gray-200 text-gray-600 px-2 py-1"
+    class="inline-block md:hidden w-10 h-8 bg-gray-200 text-gray-600 px-2 py-1"
   >
     <svg
       fill="currentColor"
@@ -32,17 +32,16 @@
     >
   </button>
   <nav
-    class="absolute md:relative top-16 left-0 md:top-0 z-20 md:flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg-white shadow-md rounded-lg md:rounded-none md:shadow-none md:bg-transparent p-6 pt-0 md:p-0"
+    class="bg-blend-hard-light text-gray-800 absolute md:relative top-16 left-0 md:top-0 z-20 md:flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg-white shadow-md rounded-b-lg md:rounded-none md:shadow-none md:bg-transparent px-6 py-4 md:p-0"
     class:flex={mobileMenuOpen}
     class:hidden={!mobileMenuOpen}
     on:keydown={() => {
       mobileMenuOpen = false;
     }}
   >
-    <a href="/" class="block py-1 text-indigo-600 hover:underline">Home</a>
-    <a href="#" class="block py-1 text-gray-600 hover:underline">About us</a>
-    <a href="#" class="block py-1 text-gray-600 hover:underline">Services</a>
-    <a href="#" class="block py-1 text-gray-600 hover:underline">Blog</a>
-    <a href="#" class="block py-1 text-gray-600 hover:underline">Contact</a>
+    <a href="/" class="block py-1 hover:underline">Home</a>
+    <a href="/" class="block py-1 hover:underline">About us</a>
+    <a href="/" class="block py-1 hover:underline">Services</a>
+    <a href="/" class="block py-1 hover:underline">Contact</a>
   </nav>
 </header>
